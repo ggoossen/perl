9a361303293e5c1059fd9b9dc6ea7bc4eccb1afa
Gerard Goossen <gerard@tty.nl>
2009-10-27 16:09:32 +0100

    constant folding using instructions

--- op.c
+++ op.c
@@ -2502,6 +2502,7 @@ S_fold_constants(pTHX_ register OP *o)
     OP *old_next;
     SV * const oldwarnhook = PL_warnhook;
     SV * const olddiehook  = PL_diehook;
+    CODESEQ* codeseq;
     COP not_compiling;
     dJMPENV;
 
@@ -2564,6 +2565,9 @@ S_fold_constants(pTHX_ register OP *o)
     o->op_next = 0;
     PL_op = curop;
 
+    codeseq = new_codeseq();
+    compile_op(PL_op, codeseq);
+
     oldscope = PL_scopestack_ix;
     create_eval_scope(G_FAKINGEVAL);
 
@@ -2580,7 +2584,7 @@ S_fold_constants(pTHX_ register OP *o)
 
     switch (ret) {
     case 0:
-	CALLRUNOPS(aTHX);
+	run_exec_codeseq(codeseq);
 	sv = *(PL_stack_sp--);
 	if (o->op_targ && sv == PAD_SV(o->op_targ))	/* grab pad temp? */
 	    pad_swipe(o->op_targ,  FALSE);
@@ -2608,6 +2612,7 @@ S_fold_constants(pTHX_ register OP *o)
     PL_warnhook = oldwarnhook;
     PL_diehook  = olddiehook;
     PL_curcop = &PL_compiling;
+    free_codeseq(codeseq);
 
     if (PL_scopestack_ix > oldscope)
 	delete_eval_scope();
