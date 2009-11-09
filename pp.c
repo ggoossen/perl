2679ab7d24634169cdf967982489ba8735ee394a
Gerard Goossen <gerard@ggoossen.net>
2009-11-09 17:36:18 +0100

    fix "state"

--- pp.c
+++ pp.c
@@ -5987,9 +5987,9 @@ PP(pp_once)
     if (SvPADSTALE(sv)) {
 	/* First time. */
 	SvPADSTALE_off(sv);
-	RUN_SET_NEXT_INSTRUCTION(cLOGOP->op_other_instr);
 	return NORMAL;
     }
+    RUN_SET_NEXT_INSTRUCTION(cLOGOP->op_other_instr);
     return NORMAL;
 }
 
