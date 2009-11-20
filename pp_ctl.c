c4ae7fe95e586fa955a49490ffd76e895977ebe9
Gerard Goossen <gerard@ggoossen.net>
2009-11-20 17:05:24 +0100

    throw "parse errors" generate by finish_optree

--- pp_ctl.c
+++ pp_ctl.c
@@ -3185,10 +3185,13 @@ S_doeval(pTHX_ int gimme, OP** startop, CV* outside, U32 seq)
 
     if (yystatus || PL_parser->error_count || !PL_eval_root) {
 	SV **newsp;			/* Used by POPBLOCK. */
-	PERL_CONTEXT *cx = NULL;
-	I32 optype;			/* Used by POPEVAL. */
+	PERL_CONTEXT *cx;
+	I32 optype;			/* Might be reset by POPEVAL. */
 	SV *namesv = NULL;
 	const char *msg;
+      parse_error:
+	cx = &cxstack[cxstack_ix];
+	optype = 0;
 
 	PERL_UNUSED_VAR(newsp);
 	PERL_UNUSED_VAR(optype);
@@ -3244,11 +3247,6 @@ S_doeval(pTHX_ int gimme, OP** startop, CV* outside, U32 seq)
 	PUTBACK;
 	return FALSE;
     }
-    CopLINE_set(&PL_compiling, 0);
-    if (startop) {
-	*startop = PL_eval_root;
-    } else
-	SAVEFREEOP(PL_eval_root);
 
     /* Set the context for this new optree.
      * Propagate the context from the eval(). */
@@ -3260,6 +3258,15 @@ S_doeval(pTHX_ int gimme, OP** startop, CV* outside, U32 seq)
 	scalar(PL_eval_root);
 
     finish_optree(PL_eval_root);
+
+    if (PL_parser->error_count) /* finish_optree might have generated new error */
+	goto parse_error;
+
+    CopLINE_set(&PL_compiling, 0);
+    if (startop) {
+	*startop = PL_eval_root;
+    } else
+	SAVEFREEOP(PL_eval_root);
 
     DEBUG_x(dump_eval());
 
