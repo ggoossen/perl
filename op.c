2e9936e4892ba12bcc9c1df1a556b6d8a5f2655f
Gerard Goossen <gerard@ggoossen.net>
2009-11-23 14:31:32 +0100

    add some ARGS_ASSERT

--- op.c
+++ op.c
@@ -1577,6 +1577,8 @@ Perl_finish_optree(pTHX_ OP* o)
 {
     COP* oldcop = PL_curcop;
 
+    PERL_ARGS_ASSERT_FINISH_OPTREE;
+
     S_finished_op_check(o);
     
     PL_curcop = oldcop;
@@ -6824,7 +6826,7 @@ Perl_ck_anoncode(pTHX_ OP *o)
 OP *
 Perl_ck_negate(pTHX_ OP *o)
 {
-    PERL_ARGS_ASSERT_CK_ANONCODE;
+    PERL_ARGS_ASSERT_CK_NEGATE;
 
     if (cUNOPo->op_first->op_type == OP_CONST)
 	cUNOPo->op_first->op_private = ~OPpCONST_STRICT;
